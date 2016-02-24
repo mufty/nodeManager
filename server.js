cec1846e8156bb8a5eb30c51ae3221b204a19a0e
Michal Gubriansky <gubriansky@sernato.sk>
2016-02-24 21:33:28 +0100

    file upload test

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var multer  = require('multer');
+
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'uploads/');
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	}
+})
+
+var upload = multer({ storage: storage })
 
 app.get('/', function (req, res) {
+	console.log( req );
+})
+
+app.post('/upload', upload.any(), function (req, res) {
 	console.log( req );
 })
 
